2f85a604facb53644c6c678e4e0db5b927aca6ef
Joohee <ljh0667@gmail.com>
2023-08-24 14:11:13 +0900

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/**/*.{js,ts,jsx,tsx}', // 적용할 js파일 경로 지정
+    './src/app/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'my-color': '#00FF7F',
+      },
+    },
+  },
+  plugins: [],
+};
